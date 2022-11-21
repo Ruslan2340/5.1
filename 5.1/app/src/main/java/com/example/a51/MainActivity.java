00b0439c78e755965773eda1c06cd9cadec89df5
Ruslan2340 <118757040+Ruslan2340@users.noreply.github.com>
2022-11-21 23:41:11 +0300

    Add files via upload

---
+++ 5.1/app/src/main/java/com/example/a51/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a51;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView =(TextView) findViewById(R.id.textView);
+    }
+
+    public  void btnClick(View view) {
+        textView.setText(R.string.tvRegInfo);
+    }
+}
